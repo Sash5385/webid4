2b7bed5bf661468afdbd4479ffbb7f62cb76717a
Sash5385 <Sash5385@gmail.com>
2026-06-02 14:25:44 +0300

    fix: correct messagingSenderId in admin service worker

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -7,7 +7,7 @@ firebase.initializeApp({
   databaseURL: "https://id4drive-booking-44182-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "id4drive-booking-44182",
   storageBucket: "id4drive-booking-44182.firebasestorage.app",
-  messagingSenderId: "815176240566",
+  messagingSenderId: "815176240686",
   appId: "1:815176240686:web:1cf54d6c465420230199bf"
 })
 
